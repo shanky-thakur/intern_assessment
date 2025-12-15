8e7958d2f3d9327e99fd6b153a08fe719a65ecad
shanky <128723278+shanky-thakur@users.noreply.github.com>
2025-12-15 17:43:40 +0530

    Remove node_modules from repository

---
+++ backend/database/mongo.js
@@ -0,0 +1,9 @@
+// environment variables set up
+require("dotenv").config();
+
+// import mongoose
+const mongoose = require();
+
+const connect_database = async () => {
+    const connection = await conn
+}
